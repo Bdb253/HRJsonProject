2c0a59409ae38fbf3321bea70500d4de8a69e845
Bowen <bdb253@nau.edu>
2019-01-16 15:11:39 -0700

    Update DeptDataLGS.json.php

--- DeptDataLGS.json.php
+++ DeptDataLGS.json.php
@@ -1,6 +1,6 @@
 <?php
     //google spreadsheets to json http://www.ravelrumba.com/blog/json-google-spreadsheets/
-    header('Content-type: script/javascript');
+    header('Content-type: application/json');
 
     $feed = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vROb0Zy_70qkx0aE8UD68rBFX6c7q256Dbi7dWrisx7fWf9c2FTkP-bB3-7PNv9ESZI8laMsLXgWvgv/pub?gid=1055648349&single=true&output=csv';
 
@@ -50,7 +50,7 @@
     }
 
     // Print it out as JSON
-    //echo json_encode($newArray);
-    echo $_GET['callback']. '(' . json_encode($newArray) . ');';
+    echo json_encode($newArray);
+
 
 ?>
